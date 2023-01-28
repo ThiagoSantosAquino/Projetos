4d1c48c79dd8debb0a85d7097bb1ea154ea36e78
Olá, eu sou Thiago Santos Aquino! <tarugoaquino@gmail.com>
2023-01-28 08:19:49 -0300

    Streamlit

---
+++ Aula _Streamlit/curriculo.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+'''
+#curriculo
+## Perfil
+    -Thiago Santos Aquino, Desensvolvedor Full Stack!
+    - Mestre em ensino de física
+    - Robótica Educacional
+
+# Objetivos
+
+    - Desenvolver software e plataformas WEB, Capacitar profissionais na área de TI 
+    e trabalhar com pesquisas de analises de dados. 
+
+# Experiencias
+
+    - Professor de fisica da rede Estadual de Ensino
+    - Professor de Robótica educacional
+
+# Local de trabalho
+
+ ![Colégio Estadual](https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D100057709230599&imgrefurl=https%3A%2F%2Fpt-br.facebook.com%2Fpages%2Fcategory%2FCollege---University%2FCol%25C3%25A9gio-Estadual-Presidente-Costa-e-Silva-145879322180723%2F&tbnid=Aim-KJlfL8ALnM&vet=12ahUKEwjtwezypsz8AhWSOLkGHciYD2YQMygAegQIARA7..i&docid=gLpub53Gp_TV5M&w=2048&h=1536&q=costa%20e%20silva%20na%20ribeira&ved=2ahUKEwjtwezypsz8AhWSOLkGHciYD2YQMygAegQIARA7)
+ 
+
+
+
+'''
